f82851f58347e230d763a6c81b09e2bab93a1343
Kartik Vij <vij.kartik11@gmail.com>
2018-10-10 19:23:08 +0530

    Update scanerRouter.py

--- scanerRouter.py
+++ scanerRouter.py
@@ -1,13 +1,13 @@
- #!/usr/bin/python2.7.14
- #the first import scapy moduls
+# !/usr/bin/python2.7.14
+# First import scapy moduls
 from scapy.all import *
 from scapy.layers.l2 import Ether, ARP
-#create a loop for testing every ip in localhost
-for subnetId in range(1,255):
-	for hostId in range(1,255):
-		ip="192.168."+str(subnetId) + "." +str(hostId)
-		arpRequest=Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip,hwdst="ff:ff:ff:ff:ff:ff")
-		arpResponse = srp1(arpRequest,timeout=1,verbose=0)
-		if arpResponse:
-			print "IP >> " + arpResponse.psrc + " MAC >> " + arpResponse.hwsrc
-#___________________________________________________________________________________________
+# Create a loop for testing every ip in localhost
+for subnetId in range(1, 255):
+    for hostId in range(1, 255):
+        ip = "192.168." + str(subnetId) + "." + str(hostId)
+        arpRequest = Ether(dst="ff:ff:ff:ff:ff:ff") /
+        ARP(pdst=ip, hwdst="ff:ff:ff:ff:ff:ff")
+        arpResponse = srp1(arpRequest, timeout=1, verbose=0)
+        if arpResponse:
+            print "IP >> " + arpResponse.psrc + " MAC >> " + arpResponse.hwsrc
